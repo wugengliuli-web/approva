6ef80a0462271fb9d7be221d7006197b00068cbe
wugengliuli-web <572471221@qq.com>
2020-05-30 22:12:19 +0800

    >

--- src/pages/lnitiateApprova/index.jsx
+++ src/pages/lnitiateApprova/index.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'dva'
 import styles from './index.less'
-import { Button, Input, Modal } from 'antd';
+import { Button, Input, Modal, Upload, message } from 'antd';
 import {
     PhoneOutlined,
     TeamOutlined,
     CloseCircleOutlined,
-    UserAddOutlined
-  } from '@ant-design/icons';
+    UserAddOutlined,
+    InboxOutlined
+} from '@ant-design/icons';
+import PDF from 'react-pdf-js';
+const { Dragger } = Upload;
+import axios from 'axios'
 @connect(({ test }) => ({ test }))
 class Login extends Component {
+    uploadProps = {
+        name: 'file',
+        multiple:  false,
+        showUploadList: false,
+        beforeUpload(info) {
+            const { type } = info
+            if(!/application\/pdf/.test(type)) {
+                message.error('格式不正确')
+                return Promise.resolve(info)
+            }
+            return Promise.reject(info)
+        },
+        onChange: info => {
+            console.log(info)
+        }
+    };
 
     state = {
         visible: false,
@@ -25,62 +45,68 @@ class Login extends Component {
             }
         },
         formLayOut: [
-            { id: '456', name: '', phone: '' },
-            { id: '789', name: '', phone: '' }
+            { id: Math.random() * 1000, name: '', phone: '' },
+            { id: Math.random() * 1000, name: '', phone: '' }
         ],
-        approvas: [{
-            id: 777,
-            contacts: [
-                { id: '456', name: '789', phone: '15681173138' },
-                { id: '789', name: '123456', phone: '7777' }
-            ]
-        }, {
-            id: 888,
-            contacts: [
-                { id: '456', name: 'aaa', phone: '15730652013' },
-                { id: '789', name: '12fff3456', phone: '879798' }
-            ]
-        }]  //待审批列表
+        approvas: [],  //待审批列表
+        file: 'file:///C:/Users/DELL/Desktop/西南科技大学+21届+web前端(暑期实习)_马羽.pdf'
     }
 
     componentDidMount() {
-        
+
     }
 
-    
+
 
     render() {
         return (
             <div className={styles.page}>
                 <div className={styles.head}>
                     <div className={styles.pdfWrapper}>
-                    
+                        {
+                            this.state.file ?
+                            <embed src={this.file} type="application/pdf" width="100%" height="100%"></embed>
+                            :
+                            null
+                        }
                     </div>
                     <div className={styles.approvas}>
-                        <div className={styles.approvasTitle}>待审批列表</div>
-                        <div className={styles.approvasContent}>
-                            {
-                                this.state.approvas.map((item, index) => {
-                                    return (
-                                        <div key={item.id} className={styles.approvasItemWrapper}>
-                                            <div className={styles.approvasItemTitle}>待审批组{index+1}</div>
-                                            {
-                                                item.contacts.map((key, value) => {
-                                                    return <div className={styles.approvasItemContent} key={key.phone}>
-                                                        <span className={styles.approvasName}>姓名：{key.name}</span>
-                                                        <span className={styles.approvasPhone}>手机：{key.phone}</span>
-                                                    </div>
-                                                })
-                                            }
-                                        </div>
-                                    )
-                                })
-                            }
+                        <div>
+                            <div className={styles.approvasTitle}>待审批列表</div>
+                            <div className={styles.approvasContent}>
+                                {
+                                    this.state.approvas.map((item, index) => {
+                                        return (
+                                            <div key={index} className={styles.approvasItemWrapper}>
+                                                <div className={styles.approvasItemTitle}>待审批组{index + 1}</div>
+                                                {
+                                                    item.contacts.map((key, value) => {
+                                                        return <div className={styles.approvasItemContent} key={key.phone + key.name}>
+                                                            <span className={styles.approvasName}>姓名：{key.name}</span>
+                                                            <span className={styles.approvasPhone}>手机：{key.phone}</span>
+                                                        </div>
+                                                    })
+                                                }
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className={styles.uploadPDFWrapper}>
+                            <Dragger {...this.uploadProps}>
+                                <p className="ant-upload-drag-icon">
+                                <InboxOutlined />
+                                </p>
+                                <p className="ant-upload-text">点击上传PDF文档</p>
+                            </Dragger>
                         </div>
                     </div>
                 </div>
                 <div className={styles.footer}>
-                    <Button onClick={this.showModel} className={styles.btn} size="large" type="primary">发起审批</Button>
+                    <Button onClick={this.showModel} className={styles.btn} size="large" type="primary">添加审批人</Button>
+
+                    <Button  className={styles.btn} size="large" type="primary">发起审批</Button>
                 </div>
 
                 <Modal
@@ -93,10 +119,10 @@ class Login extends Component {
                         this.state.formLayOut.map((item, index) => {
                             return (
                                 <div className={styles.formContainer} key={item.id}>
-                                    <div className={styles.formTitle}>审批人{index + 1} { index >= 2 ? <CloseCircleOutlined onClick={e => this.del(index)} className={styles.close} /> : null } </div>
+                                    <div className={styles.formTitle}>审批人{index + 1} {index >= 2 ? <CloseCircleOutlined onClick={e => this.del(index)} className={styles.close} /> : null} </div>
                                     <div className={styles.formWrapper}>
-                                        <Input onChange={ e => this.setFormLayOut(index, 'name', e.target.value) } className={styles.input} size="small" placeholder="请输入姓名" prefix={<TeamOutlined />} />
-                                        <Input onChange={ e => this.setFormLayOut(index, 'phone', e.target.value) } className={styles.input} size="small" placeholder="请输入手机号" prefix={<PhoneOutlined />} />
+                                        <Input onChange={e => this.setFormLayOut(index, 'name', e.target.value)} className={styles.input} size="small" placeholder="请输入姓名" prefix={<TeamOutlined />} />
+                                        <Input onChange={e => this.setFormLayOut(index, 'phone', e.target.value)} className={styles.input} size="small" placeholder="请输入手机号" prefix={<PhoneOutlined />} />
                                     </div>
                                 </div>
                             )
@@ -104,23 +130,23 @@ class Login extends Component {
                     }
                     {
                         this.state.formLayOut.length < 5 ?
-                        <div className={styles.addWrapper}>
-                            <UserAddOutlined onClick={this.add} className={styles.add} />
-                        </div>
-                        :
-                        null
+                            <div className={styles.addWrapper}>
+                                <UserAddOutlined onClick={this.add} className={styles.add} />
+                            </div>
+                            :
+                            null
                     }
                 </Modal>
             </div>
         )
     }
-    
+
     canNext = e => {
         setTimeout(() => {
             const disabled = !(this.state.formLayOut.every(item => {
                 return item.name && item.phone
             }))
-            const modalLayOut = {...this.state.modalLayOut}
+            const modalLayOut = { ...this.state.modalLayOut }
             modalLayOut['okButtonProps']['disabled'] = disabled
             this.setState({
                 modalLayOut
@@ -159,8 +185,21 @@ class Login extends Component {
         })
     }
 
-    onOk = e => {
-
+    //添加审批人
+    handleOk = e => {
+        const formLayOut = [...this.state.formLayOut]
+        const approvas = [...this.state.approvas] 
+        approvas.push({
+            contacts: formLayOut
+        })
+        this.setState({
+            approvas,
+            formLayOut: [
+                { id: Math.random() * 1000, name: '', phone: '' },
+                { id: Math.random() * 1000, name: '', phone: '' }
+            ],
+            visible: false
+        })
     }
 
     onCancel = e => {
